fbf522b7ff00f7dd96db6a562f772d39c3499ee1
Forest Moher <fomoher@gmail.com>
2021-05-14 18:08:57 -0700

    Add May12thChallenge file

---
+++ scripts/May12thChallenge.js
@@ -0,0 +1,10 @@
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+// The binary number returned should be a string.
+
+function addBinary(num1,num2) {
+    //write code here
+}
+
+//Output to window here:
+document.getElementById("result-box").innerHTML = ''
