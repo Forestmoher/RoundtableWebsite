6a3d2a96a06d55fb2f4dce9b76fea376bf48d82f
Forest Moher <fomoher@gmail.com>
2021-05-18 21:06:05 -0700

    Solve May 12th Coding Challenge

--- scripts/May12thChallenge.js
+++ scripts/May12thChallenge.js
@@ -1,10 +1,12 @@
 // Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
 
 // The binary number returned should be a string.
-
+ 
 function addBinary(num1,num2) {
-    //write code here
+    return (num1 + num2).toString(2);
 }
 
 //Output to window here:
-document.getElementById("result-box").innerHTML = ''
+document.getElementById("result-box").innerHTML = `512 + 200 in binary = ${addBinary(512,200)}<br>
+                                                    17 + 19 in binary = ${addBinary(17,19)}<br>
+                                                    3000 + 487 in binary = ${addBinary(3000,487)}`
