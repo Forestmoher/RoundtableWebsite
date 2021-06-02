28d83de79908d7169b1f766f19b2c49bd37aeb43
Forest Moher <fomoher@gmail.com>
2021-06-02 13:52:59 -0700

    add May 26 challenge

---
+++ scripts/May26thChallenge.js
@@ -0,0 +1,18 @@
+// Write a function that takes a two-digit number and determines if it's the largest of two possible digit swaps.
+
+//  For Example:
+//  largestSwap(27) ==> false
+//  largestSwap(43) ==> true
+
+// If 27 is our input, we should return false because swapping the digits gives us 72, and 72 > 27. On the other hand, swapping 43 gives us 34, and 43 > 34.
+
+function largestSwap(num) {
+    var swapped = num.toString().split('').reverse().join('');
+    return [swapped, swapped <= num];
+}
+
+//Output to window here:
+
+var num = 98
+
+document.getElementById("result-box").innerHTML = `${num} is larger than ${largestSwap(num)[0]}?: ${largestSwap(num)[1]}`; 
